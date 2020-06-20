99f1ddca17b6be768626f3ec5e0eeb192f86fa2b
Александр Гончаренко <g.aleks80@yandex.ru>
2020-06-20 18:48:41 +0500

    Объединил все скрипты в один файл

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
+//Код popup form
+
 /*ищем подходящий селектор во всем документе*/
+
 let buttonOpenForm = document.querySelector(".open-form-feedback"); 
 let popupForm = document.querySelector(".popup-form");
+
 /*ищем подходящий селектор в форме*/
+
 let popupClose = popupForm.querySelector(".button-close"); 
 let formFeedback = popupForm.querySelector('.form-feedback');
 let nameField = popupForm.querySelector(".name-field");
 let emailField = popupForm.querySelector(".email-field");
 
-
 /*Не все браузеры поддерживают localStorage поэтому проверим работоспособность.*/
+
 let isStorageSupport = true;
 let storage = "";
 
@@ -18,25 +23,26 @@ try {
   isStorageSupport = false;
 }
 
-  /*Обработчик событй для отображения формы */
+/*Обработчик событй для отображения формы */
 
   buttonOpenForm.addEventListener("click", (evt) => {
+
  /*Стандартное действие по умолчанию у ссылки переход на другую страницу. 
  Такое поведение нам не нужно, поэтому отключим его.*/   
+
     evt.preventDefault(); 
     popupForm.classList.add("popup-form-show");
-
     if(storage) {
       nameField.value = storage;
       emailField.focus();
-  } else {
+     } else {
+
     /*Установим фокус при открытии модального окна в поле ввода логина. */
-    nameField.focus(); 
-  }
 
-
+      nameField.focus(); 
+    }
   });
-
+  
   /*Обработчик событий для закрытия формы*/
 
   popupClose.addEventListener("click", (evt) => {
@@ -73,4 +79,59 @@ try {
     }
   });
 
+// Код popup Товар добавлен в корзину 
+
+let buy = document.querySelectorAll(".buy-tool");
+let productAddedCart = document.querySelector(".notice-item-added-to-cart");
+let buttonClose = productAddedCart.querySelector(".button-close");
+
+
+for(let i = 0; i < buy.length; i++) {
+  let buyClick = buy[i];
+    buyClick.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      productAddedCart.classList.add("notice-item-added-to-cart-show");
+    });
+    console.log(productAddedCart);
+  }
+
+buttonClose.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  productAddedCart.classList.remove("notice-item-added-to-cart-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if(evt.keyCode === 27) {
+    if(productAddedCart.classList.contains("notice-item-added-to-cart-show")) {
+     evt.preventDefault(); 
+      productAddedCart.classList.remove("notice-item-added-to-cart-show");
+    }
+  }
+});
+
+// Popup Map
+
+let mapsLink = document.querySelector(".maps-link");
+let popupMap = document.querySelector(".maps-popup");
+let buttonCloseMap = popupMap.querySelector(".button-close");
+
+
+mapsLink.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  popupMap.classList.add("maps-popup-show");
+});
+
+buttonCloseMap.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("maps-popup-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if(evt.keyCode === 27) {
+    if(popupMap.classList.contains("maps-popup-show")) {
+     evt.preventDefault(); 
+      popupMap.classList.remove("maps-popup-show");
+    }
+  }
+});
 
