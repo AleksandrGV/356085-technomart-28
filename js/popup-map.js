dffce6507897947d337670b8f3977c69e8ba7f3f
Александр Гончаренко <g.aleks80@yandex.ru>
2020-06-20 14:51:28 +0500

    Добавил карточкам плавное появление box-shadow при наведении

---
+++ js/popup-map.js
@@ -0,0 +1,23 @@
+let mapsLink = document.querySelector(".maps-link");
+let popupMap = document.querySelector(".maps-popup");
+let buttonClose = popupMap.querySelector(".button-close");
+
+
+mapsLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupMap.classList.add("maps-popup-show");
+});
+
+buttonClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("maps-popup-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if(evt.keyCode === 27) {
+    if(popupMap.classList.contains("maps-popup-show")) {
+     evt.preventDefault(); 
+      popupMap.classList.remove("maps-popup-show");
+    }
+  }
+});
